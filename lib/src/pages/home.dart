4e9e82eb985ca720f46d95db0f8665ac4065275e
Bryan Parada <bryan.parada.cea@gmail.com>
2023-02-08 15:50:40 -0300

    FA32 - Visual indicator when SocServ is available

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+import 'package:band_names/services/socket_service.dart';
 import 'package:band_names/src/models/band.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -22,11 +24,22 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    final socketService = Provider.of<SocketService>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('BandNames', style: TextStyle(color: Colors.black87)),
         backgroundColor: Colors.white,
-        elevation: 1
+        elevation: 1,
+        actions: [
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: socketService.serverStatus == ServerStatus.Online
+            ? Icon( Icons.check_circle, color: Colors.blue[300])
+            : Icon( Icons.offline_bolt, color: Colors.red),
+          )
+        ],
       ),
       body: ListView.builder(
         itemCount: bands.length,
